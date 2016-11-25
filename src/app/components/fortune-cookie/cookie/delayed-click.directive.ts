83eed4580614f125b6602ba6be6db14511904f0a
nathanielkess <nathaniel@nathanielkessler.com>
2016-11-24 22:01:24 -0500

    styles

--- src/app/components/fortune-cookie/cookie/delayed-click.directive.ts
+++ src/app/components/fortune-cookie/cookie/delayed-click.directive.ts
@@ -4,28 +4,56 @@ import { Directive, ElementRef, HostListener, Renderer } from '@angular/core';
   selector: '[dalayedClick]'
 })
 export class DelayedClickDirective {
-  private _defaultColor = 'red';
+  
+  private isHover = false;
+  private isClickable = false;
 
   constructor(private el: ElementRef, private renderer: Renderer) { }
+    
+  ngOnInit() { }
 
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.warmingUp();
+    this.isHover = true;
+    this.warmUp();
   }
   @HostListener('mouseleave') onMouseLeave() {
-    //this.highlight(null);
-    this.clear();
+    this.isHover = false;
+    this.isClickable = false;
+    this.cancel();
+  }
+  @HostListener('click') onClick() {
+    if(this.isClickable){
+      console.log('successful click - who FORTUNE!!!');
+    }
   }
 
 
 
+  
 
-  private warmingUp() {
-    this.renderer.setElementStyle(this.el.nativeElement, 'backgroundColor', 'yellow');
-    this.renderer.setElementClass(this.el.nativeElement, 'warmUp', true)
+
+
+  private warmUp() {
+    console.log('warming up');
+    this.renderer.setElementClass(this.el.nativeElement, 'warm', true);
+    this.makeHot();
   }
 
-  private clear(){
+  private makeHot(){
+    
+    window.setTimeout(() => {
+      if(this.isHover){
+        console.log('its hot now!!');
+        this.isClickable = true;  
+        this.renderer.setElementClass(this.el.nativeElement.children[0], 'hot', true);
+      }
+    }, 1000);
+  }
 
+  private cancel(){
+    console.log('cancel');
+    this.renderer.setElementClass(this.el.nativeElement, 'warm', false);
+    this.renderer.setElementClass(this.el.nativeElement.children[0], 'hot', false);
   }
 }
